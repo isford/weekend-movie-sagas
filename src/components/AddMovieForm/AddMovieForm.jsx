c0ad9f3660b5d02ddbd6d12aa0120031e13b69e7
Ian <isford7@gmail.com>
2021-06-19 10:17:07 -0500

    Still working on movie genre form

--- src/components/AddMovieForm/AddMovieForm.jsx
+++ src/components/AddMovieForm/AddMovieForm.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { useHistory } from 'react-router';
+import {useState} from 'react'
 
 export default function AddMovieForm() {
+
+    const history = useHistory();
+    const [genre, setGenre] = useState(0);
+    const goHome = () => {
+        console.log('Cancel button clicked')
+        history.push('/')
+        //Clear input values on cancel and submit
+    }
+
     return (
         <div>
             <form>
                 <input type="text" placeholder="Movie Title"></input>
                 <input type="text" placeholder="Movie Poster URL"></input>
                 <input type="text" placeholder="Movie Description"></input>
-                <select name="genres" id="genres">
+                <select name="genres" id="genres" onChange={(event) => setGenre(event.target.value)}>
+                    <option value ="0">Select A Genre</option>
                     <option value="1">Adventure</option>
                     <option value="2">Animated</option>
                     <option value="3">Biographical</option>
@@ -23,7 +35,15 @@ export default function AddMovieForm() {
                     <option value="13">Superhero</option>
                 </select>
             </form>
-            <button>Cancel</button>
+            <h2>TEST DATA</h2>
+            <p>TITLE:The Lord of the Rings: The Fellowship of the Ring</p>
+            <p>POSTER: https://images-na.ssl-images-amazon.com/images/I/81EBp0vOZZL._AC_SL1329_.jpg</p>
+            <p>DESCRIPTION: A meek Hobbit from the Shire
+                 and eight companions set out on a 
+                 journey to destroy the powerful One Ring 
+                 and save Middle-earth from the Dark Lord Sauron.</p>
+
+            <button onClick={goHome} >Cancel</button>
             <button>Save</button>
         </div>
     )
